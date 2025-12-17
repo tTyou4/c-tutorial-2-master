de7fa9633704a961ec4882211c71aa5d17e6d0d0
tTyou4 <daniil.pronin08@gmail.com>
2025-12-17 12:39:07 +0200

    Add files via upload

---
+++ lab05_1/lab05_1/lab05_1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+
+std::string encryptCaesar(std::string text, int key) {
+    std::string result = "";
+
+    key = key % 26;
+
+    for (char& c : text) {
+        if (isupper(c)) {
+            result += char(int(c + key - 65) % 26 + 65);
+        }
+        else if (islower(c)) {
+            result += char(int(c + key - 97) % 26 + 97);
+        }
+        else {
+            result += c;
+        }
+    }
+    return result;
+}
+
+int main() {
+    std::string text;
+    int key;
+
+    std::cout << "Enter the string to encrypt: ";
+    std::getline(std::cin, text);
+    std::cout << "Enter the key: ";
+    std::cin >> key;
+
+    std::string encrypted = encryptCaesar(text, key);
+    std::cout << "Encrypted string: " << encrypted << std::endl;
+
+    return 0;
+}
---
+++ lab05_2/lab05_2/lab05_2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+
+void bruteForceCaesar(std::string ciphertext) {
+    std::cout << "Decryption attempt:" << std::endl;
+
+    for (int key = 1; key < 26; ++key) {
+        std::string decrypted = "";
+        for (char c : ciphertext) {
+            if (isupper(c)) {
+                decrypted += char((c - 'A' - key + 26) % 26 + 'A');
+            }
+            else if (islower(c)) {
+                decrypted += char((c - 'a' - key + 26) % 26 + 'a');
+            }
+            else {
+                decrypted += c;
+            }
+        }
+        std::cout << "Key " << key << ": " << decrypted << std::endl;
+    }
+}
+
+int main() {
+    std::string ciphertext;
+
+    std::cout << "Enter the encrypted string: ";
+    std::getline(std::cin, ciphertext);
+
+    bruteForceCaesar(ciphertext);
+
+    return 0;
+}
