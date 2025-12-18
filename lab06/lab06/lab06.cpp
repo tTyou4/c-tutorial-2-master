def862ef8f7856b3e37f3230016b17f114ed00d8
tTyou4 <daniil.pronin08@gmail.com>
2025-12-18 15:22:42 +0200

    Add files via upload

---
+++ lab06/lab06/lab06.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void shuffle(int arr[], int n) {
+    for (int i = n - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        swap(&arr[i], &arr[j]);
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    srand(time(NULL));
+
+    int data[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int n = sizeof(data) / sizeof(data[0]);
+
+    printArray(data, n);
+    shuffle(data, n);
+    printArray(data, n);
+
+    return 0;
+}
